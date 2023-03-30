cef10134cc51bb23ee0256b60f8813ca116683d9
Umidxon Po'latxonov <umidxondeveloper@gmail.com>
2023-03-30 22:41:13 +0500

    fix

--- Express/express.js
+++ Express/express.js
@@ -1,29 +1,49 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const fs = require("fs");
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(express.static("public"));
 
+// get data
 app.get("/form", (req, res) => {
   res.sendFile(__dirname + "/" + "form.html");
 });
 
-app.get("/get", (req, res) => {
+// app.get("/get", (req, res) => {
+//   response = {
+//     first_name: req.query.first_name,
+//     last_name: req.query.last_name,
+//   };
+//   console.log(response);
+//   res.end(JSON.stringify(response));
+// });
+
+// post data
+app.post("/post", urlencodedParser, (req, res) => {
   response = {
-    first_name: req.query.first_name,
-    last_name: req.query.first_name,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
   };
   console.log(response);
   res.end(JSON.stringify(response));
+  let firstName = req.body.first_name
+  let lastName = req.body.last_name
+
+  let data = `${firstName} ${lastName}`
+  fs.writeFile("data.html", data, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/data", (req, res) => {
+  res.sendFile(__dirname + "/" + "data.html");
 });
 
-app.get("/api", (req, res) => {
-  res.send("API page");
-});
-
-const server = app.listen(8080, () => {
-  console.log("Server listening at http://localhost:8080");
+const server = app.listen(2000, () => {
+  console.log("Server is running at http://localhost:2000");
 });
--- REST-API/api.js
+++ REST-API/api.js
@@ -2,15 +2,31 @@ var express = require("express");
 var app = express();
 var fs = require("fs");
 
-app.get("/listUsers", function (req, res) {
-  fs.readFile(__dirname + "/" + "users.json", "utf8", function (err, data) {
+app.get("/listUsers", (req, res) => {
+  fs.readFile(__dirname + "/" + "users.json", "utf8", (err, data) => {
     console.log(data);
     res.end(data);
   });
 });
 
-var server = app.listen(8081, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log("Example app listening at http://%s:%s", host, port);
+let user = {
+  user4: {
+    name: "mohit",
+    password: "password4",
+    profession: "teacher",
+    id: 4,
+  },
+};
+
+app.post("/addUser", (req, res) => {
+  fs.readFile(__dirname + "/" + "users.json", "utf-8", (err, data) => {
+    data = JSON.parse(data);
+    data["user4"] = user["user4"];
+    console.log(data);
+    res.end(JSON.stringify(data));
+  })
+})
+
+var server = app.listen(2020, () => {
+  console.log("Server running at http://localhost:2020");
 });
